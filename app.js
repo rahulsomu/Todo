cc3ccd47eb77b4405ff7332fdb5354aecf2e51a4
rahul rana <rana.rahul.1998@gmail.com>
2021-05-08 02:26:37 +0530

    version 1

--- app.js
+++ app.js
@@ -1,123 +1,93 @@
-let todoItem=document.querySelectorAll('.todo-item');
-let todoList=document.querySelector('.todo-list');
-// let checked=false;
-let todoBtn=document.querySelector('.todo-btn');
-let dummy=document.querySelector('.dummy');
-let inputField=document.querySelector('.input-field');
+let todoItem = document.querySelectorAll('.todo-item');
+let todoList = document.querySelector('.todo-list');
+
+let todoBtn = document.querySelector('.todo-btn');
+let dummy = document.querySelector('.dummy');
+let inputField = document.querySelector('.input-field');
 var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ':'+today.getSeconds();
+var time =
+  today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+var complete = document.querySelector('.time');
+let togglebtn = document.querySelector('.toggle-btn');
+let todoContainer = document.querySelector('.todo-container');
+let body = document.querySelector('body');
+let theme = document.querySelector('.theme');
 
-
-
-
-
-// if(window.localStorage.getItem('todo')){
-// // console.log(window.localStorage.getItem('todo'));
-// let tmp=window.localStorage.getItem('todo');
-// tmp=tmp.split('#');
-// console.log(tmp);
-todoItem.forEach(item=>{
-    console.log(item);
-    todoList.insertAdjacentHTML('afterbegin',`
+todoItem.forEach(item => {
+  console.log(item);
+  todoList.insertAdjacentHTML(
+    'afterbegin',
+    `
     <div class="todo-item" onclick="test(this)"><p>${item}</p>
-    </div>`);
-})
+    </div>`
+  );
+});
 
-
-function test(item){
-// console.log(e);
-if(!item.classList.contains('checked')){
+function test(item) {
+  if (!item.classList.contains('checked')) {
     item.classList.add('checked');
-    item.style.backgroundColor='#0C7C59';
-    
-   
-    
-    // checked=true;
-    // console.log('color changed');
-}
-else{
-//    let tmp=window.localStorage.getItem('todo');
-//    tmp=tmp.split('#');
+    item.style.backgroundColor = '#0C7C59';
 
-//    let str=item.getElementsByTagName('p')[0].innerText;
+    console.log(time);
+  } else {
+    item.style.transform = 'translateX(500px)';
+    console.log(item.innerText);
+    item.style.height = '0px';
+    item.innerText = '';
+    item.style.marginBottom = '0';
+    item.style.marginTop = '0';
 
-// let arr=tmp.filter(em=>em!=str.toString());
-//    window.localStorage.setItem('todo',arr.join('#'));
-item.style.transform='translateX(500px)';
-console.log(item.innerText);
-item.style.height='0px';
-item.innerText='';
-item.style.marginBottom='0';
-item.style.marginTop='0';
-
-item.style.opacity='0';
-item.style.transform='scale(0)';
-item.style.padding='0';
-// item.remove();
-    
-    
-   
-
-}
+    item.style.opacity = '0';
+    item.style.transform = 'scale(0)';
+    item.style.padding = '0';
+  }
 }
 
-window.addEventListener('keypress',function(e){
-    var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ':'+today.getSeconds();
-    
-    if(inputField.value!=''){
-        if(e.keyCode==13){
-            // dummy.classList.add('hide');
-            todoList.insertAdjacentHTML('afterbegin',`
+window.addEventListener('keypress', function (e) {
+  var today = new Date();
+  var time =
+    today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
+
+  if (inputField.value != '') {
+    if (e.keyCode == 13) {
+      todoList.insertAdjacentHTML(
+        'beforeend',
+        `
             <div class="todo-item" onclick="test(this)"><p> ${inputField.value}</p><p class='time'>${time}</p>
-            </div>`);
-            inputField.value='';
-        }
-
+            </div>`
+      );
+      inputField.value = '';
     }
-    
+  }
+});
 
+todoBtn.addEventListener('click', function () {
+  var today = new Date();
+  var time =
+    today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds();
 
-})
-
-todoBtn.addEventListener('click',function(){
-    var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ':'+today.getSeconds();
-   
-
-    
-
-    todoBtn.classList.toggle('rotate');
-    if(inputField.value!=""){
-        // dummy.classList.add('hide');
-        todoList.insertAdjacentHTML('afterbegin',`
+  todoBtn.classList.toggle('rotate');
+  if (inputField.value != '') {
+    todoList.insertAdjacentHTML(
+      'afterbegin',
+      `
         <div class="todo-item" onclick="test(this)"><p>${inputField.value}</p><p class='time'>${time}</p>
-        </div>`);
-       
-        // if(window.localStorage.getItem('todo')){
-        //     let tmp=window.localStorage.getItem('todo');
-        //     tmp=tmp+'#'+inputField.value;
-        //     window.localStorage.setItem('todo',tmp);
-        //     console.log(tmp);
-    
-        // }
-        // else{
-        //     window.localStorage.setItem('todo',inputField.value);
-    
-            
-        // }
-        inputField.value='';
+        </div>`
+    );
 
+    inputField.value = '';
+  }
+});
 
-    }
-   
-   
-   
-
-})
-
-
-
-
-
-
+togglebtn.addEventListener('click', () => {
+  todoContainer.classList.toggle('light');
+  if (todoContainer.classList.contains('light')) {
+    theme.innerHTML = '<i class="fas fa-moon"></i>';
+    body.style.backgroundColor = '#0C7C59';
+    theme.style.color = '#F383A6';
+  } else {
+    theme.innerHTML = '<i class="fas fa-sun"></i>';
+    body.style.backgroundColor = '#BAC1B8';
+    theme.style.color = 'rgba(255, 255, 255, 0.329)';
+  }
+});
